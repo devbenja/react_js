d1c134197e3deec803e0b4265bf02ff21514f8f0
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-03-20 22:08:47 -0600

    React JS - Imprimir variables y Agregar estilos en React

--- 03-counter-app/src/App.jsx
+++ 03-counter-app/src/App.jsx
@@ -1,10 +1,23 @@
+import './app.css';
 
-export const App = ( { nombre } ) => {
+const lastName = 'Carías';
 
+const newMessage = {
+  message: 'Print Object in React',
+  author: 'Salvador',
+};
+
+const getResult = () => 'This is a Function';
+
+
+export const App = ( { name } ) => {
   return (
-    <div>Hola { nombre }</div>
+    <div>
+      <h3>{ `Hola ${ name } ${ lastName }` }</h3>
+      <code>{ JSON.stringify( newMessage ) }</code>
+      <p>{ getResult() }</p>
+    </div>
   );
-
 };
 
 
